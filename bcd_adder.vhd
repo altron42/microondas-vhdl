e562aa081f3011801d7c614776409ae44f057393
Micael Pimentel <micael.pimentel1@gmail.com>
2016-07-12 22:59:39 -0400

    Somador bcd finalizado

--- bcd_adder.vhd
+++ bcd_adder.vhd
@@ -7,7 +7,7 @@ entity bcd_adder is
    port(
       a, b  : in  std_logic_vector (3 downto 0); -- entradas.
       cin : in std_logic; -- carry in
-		estouro : in std_logic; -- soma os minutos
+		estouro : in std_logic; -- indica se eh soma dos minutos
       sum : out std_logic_vector (3 downto 0); -- soma
       cout : out std_logic  -- carry out
    );
@@ -21,7 +21,7 @@ begin
       soma_temp := ('0' & a) + ('0' & b) + ("0000" & cin);
 		if estouro = '1' and soma_temp > 5 then
 		   cout <= '1';
-			sum <= resize((soma_temp + ""),4);
+			sum <= resize((soma_temp + "01010"),4);
       elsif(soma_temp > 9) then
          cout <= '1';
          sum <= resize((soma_temp + "00110"),4);
